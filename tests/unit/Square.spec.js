fd7fb9a5de70efa5c0712b3f8cb2e28fb74791ec
jesper <jesper.jansen@student.fontys.nl>
2021-11-22 13:21:47 +0100

    Basic test using jest

---
+++ tests/unit/Square.spec.js
@@ -0,0 +1,42 @@
+import { mount } from "@vue/test-utils";
+import Square from "@/components/Square"
+
+//Group of tests
+describe("Square.vue", async () => {
+    //It == testBlock
+    it("renders number when given a square", () => {
+        //The component that you wnat to test
+        const wrapper = mount(Square, {
+            propsData: {
+                square: {
+                    correct: true,
+                    id: 0,
+                    value: 1,
+                    x: 0,
+                    y: 0,
+                },
+                value: 0
+            }
+        })
+        //The assertion
+        expect(wrapper.text()).toContain("1")
+    })
+
+    //Test written for click events
+    it("Change Value of the square", async () => {
+        const wrapper = mount(Square, {
+            propsData: {
+                square: {
+                    correct: true,
+                    id: 0,
+                    value: 1,
+                    x: 0,
+                    y: 0,
+                },
+                value: 0
+            }
+        })
+        await wrapper.find('b-button').trigger('click')
+        expect(wrapper.text()).toContain("0")
+    })
+})
---
+++ tests/unit/example.spec.js
@@ -0,0 +1,16 @@
+/*import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+//Group of tests
+describe('HelloWorld.vue', () => {
+  //It == testBlock 
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    //The component that you wnat to test
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    //The assertion
+    expect(wrapper.text()).toMatch(msg)
+  })
+})*/
